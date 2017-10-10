bf102b2b1f7974c0c4f2e091e1c54440edbaab3d
MenNoWar <mennowar@outlook.com>
2017-10-10 17:45:26 +0200

    added new Methods to Schema:  - GetSchemaForType: generates a Schema for a class  - CreateSchemaForType: generates and creates the schema for a class  - CreateSchemaForAssembly: generates and creates the schema for all classes in an Assembly

--- Chino.Sdk/Schema/Schema.cs
+++ Chino.Sdk/Schema/Schema.cs
@@ -17,12 +17,24 @@ namespace Chino.Sdk
     using RestSharp;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
     /// <summary>
     /// Defines the <see cref="Schema" />
     /// </summary>
     public class Schema
     {
+        public class OnSchemaCreatedEventArgs
+        {
+            public Schema Schema { get; set; }
+            public int Total { get; set; }
+            public int Index { get; set; }
+        }
+
+        public delegate void OnSchemaCreatedDelegate(OnSchemaCreatedEventArgs args);
+        public static event OnSchemaCreatedDelegate OnSchemaCreated = null;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Schema"/> class.
         /// </summary>
@@ -116,11 +128,112 @@ namespace Chino.Sdk
         }
 
         /// <summary>
-        /// The Delete
+        /// Creates a <see cref="Schema"/> for all classes in the assembly, regarding the <see cref="Attributes.IgnorePropertyAttribute "/> and <see cref="Attributes.ChinoAttribute"/>
         /// </summary>
-        /// <param name="client">The <see cref="RestClient"/></param>
-        /// <param name="schemaId">The <see cref="string"/></param>
-        /// <param name="disableOnly">The <see cref="bool"/></param>
+        /// <param name="client">the <see cref="RestSharp.RestClient"/> to use for communication</param>
+        /// <param name="repositoryId">the <see cref="Repository.Id"/> of the <see cref="Repository"/> to store the generated <see cref="Schema"/></param>
+        /// <param name="assembly">The <see cref="System.Type.Assembly"/> that contains the classes</param>
+        /// <param name="useClassNameAsDescription">Indicated whether the ClassName should be used as primary source, or the given description in the <see cref="Attributes.ChinoAttribute"/></param>
+        public static void CreateSchemaForAssembly(RestClient client, string repositoryId, Assembly assembly, bool useClassNameAsDescription = false)
+        {
+            var typeList = assembly.GetTypes(); // .Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal)).ToArray();
+            var idx = 1;
+            var count = typeList.Count();
+            foreach (var t in typeList)
+            {
+                var cAttrib = t.GetCustomAttribute<Attributes.ChinoAttribute>();
+                if (cAttrib != null && cAttrib.Generate)
+                {
+                    var description = string.IsNullOrEmpty(cAttrib.Description) ? t.FullName : cAttrib.Description;
+                    if (useClassNameAsDescription)
+                    {
+                        description = t.Name;
+                    }
+
+                    var schemaResponse = CreateSchemaForType(client, repositoryId, description, t);
+                    OnSchemaCreated?.Invoke(new OnSchemaCreatedEventArgs
+                    {
+                        Index = idx,
+                        Schema = schemaResponse,
+                        Total = count
+                    });
+
+                    idx++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a Schema for an <see cref="System.Type"/>.
+        /// </summary>
+        /// <param name="client">the <see cref="RestSharp.RestClient"/> to use for communication</param>
+        /// <param name="repositoryId">the <see cref="Repository.Id"/> of the <see cref="Repository"/> to store the generated <see cref="Schema"/></param>
+        /// <param name="classType">the <see cref="System.Type"/> to create the schema for</param>
+        /// <returns>the new generated <see cref="Schema"/></returns>
+        public static Schema CreateSchemaForType(RestClient client, string repositoryId, Type classType)
+        {
+            return CreateSchemaForType(client, repositoryId, classType.Name, classType);
+        }
+
+        /// <summary>
+        /// Creates a Schema for a <see cref="System.Type"/>.
+        /// </summary>
+        /// <param name="client">the <see cref="RestSharp.RestClient"/> to use for communication</param>
+        /// <param name="repositoryId">the <see cref="Repository.Id"/> of the <see cref="Repository"/> to store the generated <see cref="Schema"/></param>
+        /// <param name="description">the <see cref="Schema.Description"/> to use on the new created schema</param>
+        /// <param name="classType">the <see cref="System.Type"/> to create the schema for</param>
+        /// <returns>the new generated <see cref="Schema"/></returns>
+        public static Schema CreateSchemaForType(RestClient client, string repositoryId, string description, Type classType)
+        {
+            if (string.IsNullOrEmpty(description))
+            {
+                description = classType.Name;
+            }
+
+            var schema = GetSchemaForType(description, classType);
+            return Schema.Create(client, repositoryId, schema);
+        }
+
+        /// <summary>
+        /// Generates a <see cref="Schema"/> for the given <see cref="System.Type"/>.
+        /// </summary>
+        /// <param name="description">the <see cref="Schema.Description"/> to use on the new Schema</param>
+        /// <param name="classType">the <see cref="System.Type"/> to create the schema for</param>
+        /// <returns>a schema definition for the given class</returns>
+        public static Schema GetSchemaForType(string description, Type classType)
+        {
+            var fields = new List<SchemaField>();
+            var typeSchema = new Schema();
+
+            foreach (var property in classType.GetProperties().Where(oobj => !oobj.IsSpecialName && oobj.CanRead && oobj.CanWrite).ToList())
+            {
+                // only use properties that are sealed, should lead to strings, ints, bools, datetimes
+                var ignoreAttrib = property.GetCustomAttribute<Attributes.IgnorePropertyAttribute>();
+                if (ignoreAttrib == null)
+                {
+                    bool isSealed = property.PropertyType.IsSealed;
+                    if (isSealed)
+                    {
+                        // when PropertyName ends with "Id" let's pretend it's an indexed column
+                        var idxt = property.Name.ToUpper().EndsWith("ID");
+                        var type = Utils.CLRTypeToFieldType(property.PropertyType);
+                        SchemaField f = new SchemaField { Name = property.Name, Type = type, IsIndexed = idxt };
+                        fields.Add(f);
+                    }
+                }
+            }
+
+            typeSchema.Structure.Fields = fields;
+
+            return typeSchema;
+        }
+
+        /// <summary>
+        /// Deletes a <see cref="Schema"/> from the server
+        /// </summary>
+        /// <param name="client">The <see cref="RestClient"/> to use for communication</param>
+        /// <param name="schemaId">The Id of the Schema to delete</param>
+        /// <param name="disableOnly">Indicates whether the Schema should only be deactivated rather than deleted</param>
         public static void Delete(RestClient client, string schemaId, bool disableOnly)
         {
             var uri = string.Format("/schemas/{0}", schemaId); //  {?force}{&all_content}")
@@ -155,19 +268,19 @@ namespace Chino.Sdk
         public string RepositoryId { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether IsActive
+        /// Gets or sets a value indicating whether the Schema is active
         /// </summary>
         [JsonProperty(PropertyName = "is_active")]
         public bool IsActive { get; set; }
 
         /// <summary>
-        /// Gets or sets the Created
+        /// Gets or sets the date the Schema was created
         /// </summary>
         [JsonProperty(PropertyName = "insert_date")]
         public DateTime Created { get; set; }
 
         /// <summary>
-        /// Gets or sets the Updated
+        /// Gets or sets the Schema was updated
         /// </summary>
         [JsonProperty(PropertyName = "last_update")]
         public DateTime Updated { get; set; }
